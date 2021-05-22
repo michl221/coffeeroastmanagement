2f2c19e8c5348321200da7118e46fb03bf23cc5f
Michael Boegl <michael.boelg@gmail.com>
2021-05-22 20:57:58 +0200

    Remove ChartJS Blazor library and use ChartJS directly.

---
+++ CoffeeRoastManagement/Client/wwwroot/chart.js
@@ -0,0 +1,10 @@
+﻿window.setup = (id, config) => {
+    console.log("TEST")
+    var ctx = document.getElementById(id).getContext('2d');
+    new Chart(ctx, config);
+}
+
+window.artisanprofilegraph = (id, config) => {
+    var ctx = document.getElementById(id).getContext('2d');
+    new Chart(ctx, config);
+}
