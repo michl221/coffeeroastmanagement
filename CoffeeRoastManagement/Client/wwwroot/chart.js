992dc90270458e77cf43d5e5e882bd03059eb349
Michael Boegl <michael.boelg@gmail.com>
2021-05-22 21:06:36 +0200

    Remove unused chart javascript function

--- CoffeeRoastManagement/Client/wwwroot/chart.js
+++ CoffeeRoastManagement/Client/wwwroot/chart.js
@@ -1,10 +1,4 @@
-﻿window.setup = (id, config) => {
-    console.log("TEST")
-    var ctx = document.getElementById(id).getContext('2d');
-    new Chart(ctx, config);
-}
-
-window.artisanprofilegraph = (id, config) => {
+﻿window.artisanprofilegraph = (id, config) => {
     var ctx = document.getElementById(id).getContext('2d');
     new Chart(ctx, config);
 }
